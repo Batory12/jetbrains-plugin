c9d7a7b575ad53ba591b29bb7d636f756210e79c
Batory12 <bartoldo2004@gmail.com>
2025-04-03 13:59:34 +0200

    update dependencies.
    Add git4idea as dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,8 +17,9 @@ intellij {
     version.set("2023.2.5")
     type.set("IC") // Target IDE Platform
 
-    plugins.set(listOf(/* Plugin Dependencies */))
+    plugins.set(listOf("Git4Idea"))
 }
+
 
 tasks {
     // Set the JVM compatibility versions
